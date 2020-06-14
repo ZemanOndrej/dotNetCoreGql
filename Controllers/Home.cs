ed62502d08d0a16da097746085ca5ece18e9d665
Ondrej Zeman <ozeman77@gmail.com>
2020-06-14 21:31:45 +0200

    init commit

---
+++ Controllers/Home.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace webapiPgGql.Controllers
+{
+    [ApiController]
+    [Route("/api")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return "Welcome to the api";
+        }
+    }
+}
